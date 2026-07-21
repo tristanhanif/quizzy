01664a115265d3892a6b48f9751f793e81f1badf
tristanhanif <tristanhanif1769@gmail.com>
2026-07-21 14:25:51 +0700

    fix: use --no-lint flag for next build in package.json

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,11 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // Abaikan error ESLint saat proses build di Vercel
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  // Abaikan error TypeScript saat proses build di Vercel
   typescript: {
     ignoreBuildErrors: true,
   },
