0c5de60084faa106affd0a175b929acc3278afc4
tristanhanif <tristanhanif1769@gmail.com>
2026-07-21 13:58:39 +0700

    fix: disable lint on build for next 16

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -4,10 +4,6 @@ const nextConfig = {
     // Abaikan error tipe TypeScript saat build di Vercel
     ignoreBuildErrors: true,
   },
-  eslint: {
-    // Abaikan warning/error ESLint saat build di Vercel
-    ignoreDuringBuilds: true,
-  },
 };
 
 export default nextConfig;
